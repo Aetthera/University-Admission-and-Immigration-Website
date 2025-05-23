683a4f8dc4d6a6de790d04f3d1c0b818b7c54712
Karim_F <karim.michelf@gmail.com>
2025-05-22 20:38:14 -0400

    Added the first part of the Home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import Footer from './components/Footer'
 import Header from './components/Header';
+import Hero from './components/heroSection';
 
 function App() {
   return (
     <>
     <Header />
+    <div>
+    <Hero/>
+    </div>
+    
     <Footer />
     </>
   );
