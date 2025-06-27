a44a90ea08cf5f9eb722f5417adb8dfcdb46f242
sarahdadoun01 <85398444+sarahdadoun@users.noreply.github.com>
2025-06-27 11:23:37 -0400

    added components to complete part 1 of the first section (school details and preview)

--- frontend/src/components/ApplicationTab.jsx
+++ frontend/src/components/ApplicationTab.jsx
@@ -3,6 +3,8 @@ import DropDownButton from "./DropDownButton.jsx";
 import DropdownMenu from "./DropdownMenu.jsx";
 import BasicRoundedButton from "./BasicRoundedButton";
 import { Mail, Printer } from "lucide-react";
+import SchoolDetails from "./SchoolDetails";
+import ApplicationPreviewCard from "./ApplicationPreviewCard.jsx";
 
 export default function ApplicationTab() {
     const [universities, setUniversities] = useState([]);
@@ -15,6 +17,10 @@ export default function ApplicationTab() {
     const [isProgramOpen, setIsProgramOpen] = useState(false);
 
     const [activeTab, setActiveTab] = useState("all");
+
+    const [school, setSchool] = useState(null);
+    const [application, setApplication] = useState(null);
+
 
     const containerRef = useRef();
 
@@ -66,130 +72,189 @@ export default function ApplicationTab() {
         }
     }, [selectedUniversity, selectedProgram]);
 
+    useEffect(() => {
+        fetch("/data/schoolDetails.json")
+            .then((res) => res.json())
+            .then(setSchool);
+    }, []);
+
+    useEffect(() => {
+        fetch("/data/applications.json")
+            .then((res) => res.json())
+            .then((data) => {
+                setApplication(data[0]);
+            });
+    }, []);
+
+
 
     return (
 
         <>
 
-        <p className="flex justify-center p-[60px] text-lg text-center">
-            Select your school and program <br/>to view your application details.
-        </p>
-        
-        <section className="flex flex-col gap-5 min-h-[1000px] bg-white rounded-[40px] shadow-[0_10px_40px_rgba(0,0,0,0.05)]">
+            <p className="flex justify-center p-[60px] text-lg text-center">
+                Select your school and program <br />to view your application details.
+            </p>
 
-            {/* Top - Buttons options, mail & print */}
-            <div className="flex flex-row justify-between gap-1" ref={containerRef}>
+            <section className="flex flex-col gap-5 min-h-[1000px] bg-white rounded-[40px] shadow-[0_10px_40px_rgba(0,0,0,0.05)]">
+
+                {/* Top - Buttons options, mail & print */}
+                <div className="flex flex-row justify-between gap-1" ref={containerRef}>
 
 
-                {/* TOP - Left */}
-                <div className="flex flex-row justify-start gap-1 p-4">
+                    {/* TOP - Left */}
+                    <div className="flex flex-row justify-start gap-1 p-4">
 
-                    {/* University Selector */}
-                    <div className="relative inline-block">
-                        <DropDownButton
-                            label="Select University"
-                            selected={selectedUniversity}
-                            isOpen={isUniversityOpen}
-                            onClick={() => setIsUniversityOpen(!isUniversityOpen)}
-                        />
-                        {isUniversityOpen && (
-                            <DropdownMenu
-                                items={universities}
-                                selectedItem={selectedUniversity}
-                                onSelect={(uni) => {
-                                    setSelectedUniversity(uni);
-                                    setIsUniversityOpen(false);
-                                }}
+                        {/* University Selector */}
+                        <div className="relative inline-block">
+                            <DropDownButton
+                                label="Select University"
+                                selected={selectedUniversity}
+                                isOpen={isUniversityOpen}
+                                onClick={() => setIsUniversityOpen(!isUniversityOpen)}
                             />
-                        )}
+                            {isUniversityOpen && (
+                                <DropdownMenu
+                                    items={universities}
+                                    selectedItem={selectedUniversity}
+                                    onSelect={(uni) => {
+                                        setSelectedUniversity(uni);
+                                        setIsUniversityOpen(false);
+                                    }}
+                                />
+                            )}
+                        </div>
+
+                        {/* Program Selector */}
+                        <div className="relative inline-block">
+                            <DropDownButton
+                                label="Select Program"
+                                selected={selectedProgram}
+                                isOpen={isProgramOpen}
+                                onClick={() => {
+                                    if (selectedUniversity) {
+                                        setIsProgramOpen(!isProgramOpen);
+                                    }
+                                }}
+                                disabled={!selectedUniversity}
+                            />
+                            {isProgramOpen && (
+                                <DropdownMenu
+                                    items={filteredPrograms}
+                                    selectedItem={selectedProgram}
+                                    onSelect={(program) => {
+                                        setSelectedProgram(program);
+                                        setIsProgramOpen(false);
+                                    }}
+                                />
+                            )}
+                        </div>
+
+                        {/* Detail View btn */}
+                        <BasicRoundedButton
+                            label="Detail View"
+                            isSelected={activeTab === "detailView"}
+                            onClick={() => setActiveTab(activeTab === "detailView" ? null : "detailView")}
+                        />
+
+                        {/* See All btn */}
+                        <BasicRoundedButton
+                            label="See All"
+                            isSelected={activeTab === "all"}
+                            onClick={() => setActiveTab(activeTab === "all" ? null : "all")}
+                        />
+
+
+                        {/* Drafts btn */}
+                        <BasicRoundedButton
+                            label="Drafts"
+                            isSelected={activeTab === "drafts"}
+                            onClick={() => setActiveTab(activeTab === "drafts" ? null : "drafts")}
+                        />
+
+                        {/* Saved btn*/}
+                        <BasicRoundedButton
+                            label="Saved"
+                            isSelected={activeTab === "saved"}
+                            onClick={() => setActiveTab(activeTab === "saved" ? null : "saved")}
+                        />
                     </div>
 
-                    {/* Program Selector */}
-                    <div className="relative inline-block">
-                        <DropDownButton
-                            label="Select Program"
-                            selected={selectedProgram}
-                            isOpen={isProgramOpen}
+
+
+                    {/* TOP - Right */}
+                    <div className="flex flex-row justify-end gap-1 p-4">
+                        <button
                             onClick={() => {
-                                if (selectedUniversity) {
-                                    setIsProgramOpen(!isProgramOpen);
-                                }
+                                // TODO: handle email sending logic
+                                console.log("Send email");
                             }}
-                            disabled={!selectedUniversity}
-                        />
-                        {isProgramOpen && (
-                            <DropdownMenu
-                                items={filteredPrograms}
-                                selectedItem={selectedProgram}
-                                onSelect={(program) => {
-                                    setSelectedProgram(program);
-                                    setIsProgramOpen(false);
-                                }}
-                            />
+                            className="text-black bg-white w-0"
+                            aria-label="Send to Email"
+                        >
+                            <Mail className="w-6 h-6" />
+                        </button>
+
+                        <button
+                            onClick={() => {
+                                window.print(); // basic print behavior
+                            }}
+                            className="text-black bg-white"
+                            aria-label="Print"
+                        >
+                            <Printer className="w-6 h-6" />
+                        </button>
+                    </div>
+
+                </div>
+
+                {/* PART 1 /// Top */}
+                <div className="py-[40px]">
+
+                    {application && (
+                        <p className="text-sm text-gray-500 text-center">
+                            Application created on{" "}
+                            {new Date(application.createdAt).toLocaleString(undefined, {
+                                year: "numeric",
+                                month: "long",
+                                day: "numeric",
+                                hour: "numeric",
+                                minute: "2-digit"
+                            })}
+                        </p>
+                    )}
+
+                </div>
+
+                {/* PART 1 /// Bottom */}
+                <div className="flex flex-row justify-between items-center mx-[200px]">
+                    {/* Left - School Details */}
+                    <div className="">
+                        {!school ? (
+                            <p className="text-gray-500">Loading...</p>
+                        ) : (
+                            <section className="bg-white p-6">
+                                <SchoolDetails school={school} />
+                            </section>
                         )}
                     </div>
 
-                    {/* Detail View btn */}
-                    <BasicRoundedButton
-                        label="Detail View"
-                        isSelected={activeTab === "detailView"}
-                        onClick={() => setActiveTab(activeTab === "detailView" ? null : "detailView")}
-                    />
-                    
-                    {/* See All btn */}
-                    <BasicRoundedButton
-                        label="See All"
-                        isSelected={activeTab === "all"}
-                        onClick={() => setActiveTab(activeTab === "all" ? null : "all")}
-                    />
+                    {/* Right - Application Image Preview */}
+                    <div className="w-[300px]">
+                        <ApplicationPreviewCard
+                            imageUrl="/uploads/college-application-preview.png"
+                            pdfUrl="/uploads/college-application.pdf"
+                            uploadedAt="June 27, 2025"
+                        />
 
+                    </div>
 
-                    {/* Drafts btn */}
-                    <BasicRoundedButton
-                        label="Drafts"
-                        isSelected={activeTab === "drafts"}
-                        onClick={() => setActiveTab(activeTab === "drafts" ? null : "drafts")}
-                    />
-
-                    {/* Saved btn*/}
-                    <BasicRoundedButton
-                        label="Saved"
-                        isSelected={activeTab === "saved"}
-                        onClick={() => setActiveTab(activeTab === "saved" ? null : "saved")}
-                    />
                 </div>
 
+                {/* PART 2 /// Progress Details */}
+                <div></div>
 
-
-                {/* TOP - Right */}
-                <div className="flex flex-row justify-end gap-1 p-4">
-                    <button
-                        onClick={() => {
-                            // TODO: handle email sending logic
-                            console.log("Send email");
-                        }}
-                        className="text-black bg-white w-0"
-                        aria-label="Send to Email"
-                    >
-                        <Mail className="w-6 h-6" />
-                    </button>
-
-                    <button
-                        onClick={() => {
-                            window.print(); // basic print behavior
-                        }}
-                        className="text-black bg-white"
-                        aria-label="Print"
-                    >
-                        <Printer className="w-6 h-6" />
-                    </button>
-                </div>
-
-            </div>
-
-
-
-        </section>
+            </section>
         </>
 
 
