d283a3610596a084236534cc6ddde873e5e6ed1a
Alena <alenabelovadmvn@gmail.com>
2025-07-04 10:53:49 -0400

    Create reviews.js

---
+++ frontend/src/data/reviews.js
@@ -0,0 +1,45 @@
+// src/data/reviews.js
+import JohnImg from '../assets/john.png';
+import JessicaImg from '../assets/jessica.png';
+
+export const topRowReviews = [
+  {
+    id: 1,
+    name: "Jessica Johnson",
+    image: JessicaImg,
+    review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+  },
+  {
+    id: 2,
+    name: "John Doe",
+    image: JohnImg,
+    review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+  },
+];
+
+export const bottomRowReviews = [
+  {
+    id: 3,
+    name: "John Doe",
+    image: JohnImg,
+    review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+  },
+  {
+    id: 4,
+    name: "John Doe",
+    image: JohnImg,
+    review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+  },
+  {
+    id: 5,
+    name: "John Doe",
+    image: JohnImg,
+    review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+  },
+  {
+    id: 6,
+    name: "John Doe",
+    image: JohnImg,
+    review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+  },
+];
